67e52c2525b47c8e1f5175bc733bff8d480be972
capaths <pcampaa@gmail.com>
2025-05-21 20:18:34 -0400

    feat: add products models

--- wishlists/models/product_url.py
+++ wishlists/models/product_url.py
@@ -3,6 +3,12 @@ from django.db import models
 from wishlists.models.wishlisted_product import WishlistedProduct
 
 
+class Currency(models.IntegerChoices):
+    EUR = 1, "EUR"
+    USD = 2, "USD"
+    CLP = 3, "CLP"
+
+
 class ProductURL(models.Model):
     url_host = models.CharField(max_length=255, db_index=True)
     url_path = models.CharField(max_length=255)
@@ -10,4 +16,9 @@ class ProductURL(models.Model):
         WishlistedProduct,
         on_delete=models.CASCADE,
         related_name='urls'
+    )
+    last_price = models.IntegerField()
+    last_price_currency = models.PositiveSmallIntegerField(
+        choices=Currency.choices,
+        default=Currency.CLP
     )
--- wishlists/models/wishlisted_product.py
+++ wishlists/models/wishlisted_product.py
@@ -2,11 +2,6 @@ from  django.db import models
 
 from wishlists.models.wishlist import Wishlist
 
-class Currency(models.IntegerChoices):
-    EUR = 1, "EUR"
-    USD = 2, "USD"
-    CLP = 3, "CLP"
-
 
 class WishlistedProduct(models.Model):
     wishlist = models.ForeignKey(
@@ -14,8 +9,3 @@ class WishlistedProduct(models.Model):
         on_delete=models.CASCADE,
     )
     display_name = models.CharField(max_length=255)
-    last_price = models.IntegerField()
-    last_price_currency = models.PositiveSmallIntegerField(
-        choices=Currency.choices,
-        default=Currency.CLP
-    )
