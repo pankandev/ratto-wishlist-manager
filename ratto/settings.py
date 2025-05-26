f1539eacecc78a94d0e0b5d06e9b6146626c2b30
capaths <pcampaa@gmail.com>
2025-05-25 20:03:24 -0400

    fix: add 127.0.0.1 to allowed_hosts

--- ratto/settings.py
+++ ratto/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1b3nv2qj%_d*!ak_k+5%yvqvz1yyfanj-%zl8y=^75vjy*=@m+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
